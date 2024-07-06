83f501abc6e5354dbedb4b616a6175a81f40e280
Pablo Werlang <pswerlang@gmail.com>
2024-07-06 03:21:54 -0300

    Background now call submission judge.

--- background/app.js
+++ background/app.js
@@ -2,17 +2,24 @@ import Request from './request.js';
 
 const background = {
     init: async function() {
+        console.log('Background script started');
         this.request = new Request({ url: `http://api:3000` });
 
         this.watchSubmissions();
     },
     
     watchSubmissions: async function() {
-        const id = 1;
-        const resp = await this.request.get(`submissions/${id}`, {});
-        console.log(resp);
+        const submissions = await this.request.get(`submissions/pending`, {});
+        // console.log(submissions);
+
+        if (submissions.submissions.length > 0) {
+            const submission = submissions.submissions[0];
+
+            const response = await this.request.post(`submissions/${submission.id}/judge`, {});
+            console.log(response);
+        }
             
-        setTimeout(() => this.watchSubmissions(), 1000);
+        // setTimeout(() => this.watchSubmissions(), 1000);
     },
 }
 
