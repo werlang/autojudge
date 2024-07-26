6c21e842eea4822390a87e21da8a382ecbb74f81
Pablo Werlang <pswerlang@gmail.com>
2024-07-26 10:54:16 -0300

    Load problem in dedicated page.

--- web/src/js/dashboard-single-problem.js
+++ web/src/js/dashboard-single-problem.js
@@ -1,5 +1,40 @@
+import Problem from './model/problem.js'
+
 export default {
     load: async function(id) {
-        console.log('loading problem', id);
+        const {problem} = await new Problem({ id }).get();
+        // console.log(problem);
+
+        if (!problem) {
+            location.href = '/problems';
+            return;
+        }
+
+        this.problem = problem;
+        this.render();
+    },
+
+    render: function() {
+        const inputLength = item => item && item.length ? JSON.parse(item).length : 0;
+        const getItems = item => item && item.length ? JSON.parse(item) || [] : [];
+        const io = {
+            input: getItems(this.problem.input),
+            output: getItems(this.problem.output)
+        };
+        const codes = io.input.map((_, i) => `<div class="code">
+            ${[Object.keys(io).map(key => `<div class="case">
+                <span class="label">${this.translate(key, 'problem')}</span>
+                ${io[key][i]}
+            </div>`).join('')]}
+        </div>`).join('');
+        
+        const frame = document.querySelector('#frame');
+        frame.innerHTML = `<div id="problem">
+            <h1>${this.problem.title}</h1>
+            <p>${this.problem.description}</p>
+            <h3>${this.translate('inout', 'problem', {count: inputLength(this.problem.input)})}</h3>
+            ${codes}
+        </div>`;
     }
+
 }
