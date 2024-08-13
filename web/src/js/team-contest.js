bc8d4b4251f5d55d75614be6a843761e1f9a6a6e
Pablo Werlang <pswerlang@gmail.com>
2024-08-13 00:49:44 -0300

    Add countdown to competition.

--- web/src/js/team-contest.js
+++ web/src/js/team-contest.js
@@ -10,6 +10,8 @@ export default {
         const frame = document.querySelector('#frame');
         frame.innerHTML = `
             <h1>${this.translate('contest_one', 'common')} ${this.team.contest.name}</h1>
+            <p id="contest-description"></p>
+            <div id="time-left"></div>
         `;
 
         // console.log(this.team);
@@ -41,6 +43,39 @@ export default {
         const {contest} = await promise;
         this.contest = contest;
 
+        document.querySelector('#contest-description').innerHTML = contest.description;
+
+        const startTime = new Date(contest.startTime).getTime();
+        const duration = contest.duration * 60 * 1000;
+        const timeLeft = new Date(duration - (Date.now() - startTime)).getTime();
+
+        const timeLeftDOM = document.querySelector('#time-left');
+        // contest is finished
+        if (timeLeft < 0) {
+            timeLeftDOM.innerHTML = `
+                <span id="message">${this.translate('contest.finished', 'team')}</span>
+                <div id="clock">${this.formatClock(0)}</div>
+            `;
+        }
+        else {
+            let timeLeft = new Date(duration - (Date.now() - startTime)).getTime();
+            let message = this.translate('contest.ends', 'team');
+            
+            // contest is not started yet
+            if (startTime > Date.now()) {
+                timeLeft = new Date(startTime - Date.now()).getTime();
+                message = this.translate('contest.starts', 'team');
+            }
+
+            timeLeftDOM.innerHTML = `
+                <span id="message">${message}</span>
+                <div id="clock">${this.formatClock(timeLeft)}</div>
+            `;
+
+            const clock = timeLeftDOM.querySelector('#clock');
+            this.updateClock(clock, timeLeft, () => this.build());
+        }
+
         table.clear();
         // this.contest.teams.forEach(team => {
         //     if (team.author) {
@@ -53,6 +88,29 @@ export default {
         // table.addItemEvent('click', async item => {
         //     this.show(item);
         // });
+    },
+
+    formatClock: function(time) {
+        const date = new Date(time);
+        const hours = date.getUTCHours();
+        const minutes = date.getUTCMinutes();
+        const seconds = date.getUTCSeconds();
+        return `
+            <span>${hours.toString().padStart(2, '0')}</span>:
+            <span>${minutes.toString().padStart(2, '0')}</span>:
+            <span>${seconds.toString().padStart(2, '0')}</span>
+        `;
+    },
+
+    updateClock: function(clock, timeLeft, callback) {
+        const start = Date.now();
+        clock.innerHTML = this.formatClock(timeLeft);
+        if (timeLeft > 0) {
+            setTimeout(() => this.updateClock(clock, timeLeft - (Date.now() - start), callback), 1000);
+        }
+        else {
+            if (callback) callback();
+        }
     },
 
 }
