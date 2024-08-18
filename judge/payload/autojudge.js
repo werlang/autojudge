d9a874cd50c101c0b37b1f4fb7d8339709d96cf2
Pablo Werlang <pswerlang@gmail.com>
2024-08-18 01:10:24 -0300

    Autojudge now trims stdout and response to avoid Presentation Error.

--- judge/payload/autojudge.js
+++ judge/payload/autojudge.js
@@ -100,7 +100,7 @@ fs.readdir(inputDir, async (error, inputFiles) => {
                     message: stderr,
                 });
             }
-            else if (stdout !== outputContents) {
+            else if (stdout.trim() !== outputContents.trim()) {
                 fail += 1;
                 results.push({
                     file: inputFilePath,
